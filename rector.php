eacc06aea4f3ff4f6bc88ed80f8482f1afaf0579
Gustavo Schattner <gucschattner@gmail.com>
2025-12-22 19:09:54 -0300

    style: apply coding standards to config files

--- rector.php
+++ rector.php
@@ -8,8 +8,8 @@ use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ]);
 
     $rectorConfig->sets([
@@ -17,7 +17,7 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::CODE_QUALITY,
         SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
-        LevelSetList::UP_TO_PHP_82
+        LevelSetList::UP_TO_PHP_82,
     ]);
 
 };
